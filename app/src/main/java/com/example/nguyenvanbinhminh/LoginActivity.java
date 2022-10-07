14812ecba9e098cb38e6f9a5cac80fa244cb7e43
BinhMinh0111 <binhminh.eng1199@gmail.com>
2022-10-07 09:05:45 +0700

    Done

---
+++ app/src/main/java/com/example/nguyenvanbinhminh/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.example.nguyenvanbinhminh;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    TextView tvMail;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        tvMail = findViewById(R.id.tvMail);
+
+    btnSignIn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mAuth.signInWithEmailAndPassword("minh@gmail.com", "123456")
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            tvMail.setText(user.getEmail());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+        }
+    });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword("minh@gmail.com", "123456")
+                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                              @Override
+                              public void onComplete(@NonNull Task<AuthResult> task) {
+                                  Toast.makeText(LoginActivity.this, "OKEE", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null) {
+            tvMail.setText(user.getEmail());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/nguyenvanbinhminh/SignupActivity.java
@@ -0,0 +1,46 @@
+package com.example.nguyenvanbinhminh;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignupActivity extends AppCompatActivity {
+
+    Button btn_login;
+    TextView textView, tvDeSau;
+    EditText login_email;
+
+    AppBarConfiguration appBarConfiguration;
+    NavController navController;
+    Toolbar toolbar;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
+        navController = Navigation.findNavController(this,R.id.nav_signin);
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.fullNameFragment, R.id.addressFragment,R.id.emailFragment)
+                .build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController( toolbar,navController);
+
+
+    }
+}
